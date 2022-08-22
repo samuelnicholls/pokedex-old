8c797696ac8af7c33aa6c4e1605026ee5da6b9ea
Samuel Nicholls <samuel.nicholls@hey.com>
2022-08-22 16:19:28 +0100

    Fixing config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
       'img.pokemondb.net',
     ],
-  }
+  },
+  assetPrefix: isProd ? '/pokedex/' : ''
 }
 
 module.exports = nextConfig
